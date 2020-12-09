c54ece5bb08499445128142212d8b699d74d9f6a
Emily Blair <emilyblair96@gmail.com>
2020-12-09 15:22:02 -0600

    Add writeFile piece to generate team.html

--- Develop/app.js
+++ Develop/app.js
@@ -10,19 +10,143 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+let teamMembers = [];
+
+let employeeBasics = [
+{
+    type: "list",
+    message: "Which type of team member would you like to add?",
+    choices: ["Manager", "Engineer", "Intern", "No more team members to add!"],
+    name: "employeeType", 
+},
+{
+    type: "input",
+    message: "Employee's name",
+    name: "name",
+},
+{
+    type: "input",
+    message: "Employee's id",
+    name: "id",
+},
+{
+    type: "input",
+    message: "Employee's email",
+    name: "email",
+},
+];
+
+let managerSpecifics = [
+    {
+        type: "input",
+        message: "Office number",
+        name: "officeNumber",
+    },
+];
+
+let engineerSpecifics = [
+    {
+        type: "input",
+        message: "Github username",
+        name: "githubUsername",
+    },
+];
+
+let internSpecifics = [
+    {
+        type: "input",
+        message: "School",
+        name: "school",
+    },
+];
+
+let verify = [
+    {
+    type: 'confirm',
+    name: 'verify',
+    message: 'Want to add another employee? (just hit enter for YES)?',
+    default: true,
+    },
+];
+
+function addEmployee() {
+    inquirer.prompt(employeeBasics).then((response) => {
+        let name = response.name;
+        let id = response.id;
+        let email = response.email;
+        if (response.employeeType === "Manager") {
+            inquirer.prompt(managerSpecifics).then((response) => {
+                let newEmployee = new Manager(name, id, email, response.officeNumber);
+                teamMembers.push(newEmployee);
+                console.log(teamMembers);
+                checkComplete();
+            });
+        };
+
+        if (response.employeeType === "Engineer") {
+            inquirer.prompt(engineerSpecifics).then((response) => {
+                let newEmployee = new Engineer(name, id, email, response.githubUsername);
+                teamMembers.push(newEmployee);
+                console.log(teamMembers);
+                checkComplete();
+            });
+        };
+
+        if (response.employeeType === "Intern") {
+            inquirer.prompt(internSpecifics).then((response) => {
+                let newEmployee = new Intern(name, id, email, response.school);
+                teamMembers.push(newEmployee);
+                console.log(teamMembers);
+                checkComplete();
+            });
+        };
+    //   employees.push(response.employeeList);
+    //   if (response.restart) {
+    //     addEmployee();
+    //   } else {
+    //     console.log('Your team members:', employees.join(', '));
+    //   }
+
+        //inquirer.prompt
+
+    });
+};
+
+function checkComplete() {
+    inquirer.prompt(verify).then((response) => {
+        if (response.verify) {
+            addEmployee();
+        } else {
+            console.log("Your team members: " + teamMembers)
+            let pageContent = render(teamMembers);
+            fs.writeFile(outputPath, pageContent, (err) =>
+                err 
+                ? console.error(err) 
+                : console.log('Success!')
+                );
+        }
+    });
+};
+
+addEmployee();
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+
+
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
+
+
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
