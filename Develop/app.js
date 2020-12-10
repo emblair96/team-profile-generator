08bb1f0304fd133c94c3e2b1fce1010c0ae6dbdb
Emily Blair <emilyblair96@gmail.com>
2020-12-10 15:36:11 -0600

    Add README

--- Develop/app.js
+++ Develop/app.js
@@ -10,16 +10,13 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
 let teamMembers = [];
 
 let employeeBasics = [
 {
     type: "list",
     message: "Which type of team member would you like to add?",
-    choices: ["Manager", "Engineer", "Intern", "No more team members to add!"],
+    choices: ["Manager", "Engineer", "Intern"],
     name: "employeeType", 
 },
 {
@@ -103,14 +100,6 @@ function addEmployee() {
                 checkComplete();
             });
         };
-    //   employees.push(response.employeeList);
-    //   if (response.restart) {
-    //     addEmployee();
-    //   } else {
-    //     console.log('Your team members:', employees.join(', '));
-    //   }
-
-        //inquirer.prompt
 
     });
 };
@@ -120,7 +109,6 @@ function checkComplete() {
         if (response.verify) {
             addEmployee();
         } else {
-            console.log("Your team members: " + teamMembers)
             let pageContent = render(teamMembers);
             fs.writeFile(outputPath, pageContent, (err) =>
                 err 
@@ -132,28 +120,3 @@ function checkComplete() {
 };
 
 addEmployee();
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
---
+++ README.md
@@ -0,0 +1,34 @@
+# Team Profile Generator
+
+![tested with jest](https://img.shields.io/badge/tested_with-jest-99424f.svg)
+
+## Description
+Create an HTML webpage of your employees/team members from your command line!  Using inquirer, answer a few questions about each team member that will be used to display the content on the HTML output.  
+
+## Technologies
+* HTML
+* CSS
+* [Bootstrap](https://getbootstrap.com/)
+* JavaScript
+* Node.js
+* [Inquirer.js](https://www.npmjs.com/package/inquirer)
+* [Jest](https://jestjs.io/en/)
+
+## Installation
+To install the necessary dependencies to run this application on your local machine:
+
+``` bash
+npm --i
+```
+## Usage
+
+![quick-README-generator](Assets/quick-README-generator.gif)
+
+## Areas for improvement
+I would like to revisit this project to improve the CSS on the HTML output page, as well as add validation to verify the user input. 
+
+## License
+Copyright &copy; Licensed under the MIT license.
+
+## Questions
+Contact me at emilyblair96@gmail.com with questions.
